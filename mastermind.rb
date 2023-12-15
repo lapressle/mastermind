e429974e0a7ac77bb65a0579b213a1a5b3ba75c0
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-15 00:08:35 -0500

    Set up initial variables for classes

--- mastermind.rb
+++ mastermind.rb
@@ -7,3 +7,34 @@ end
 # This sets up the pieces of the board
 class GameBoard
 end
+
+# This is each row for the game made up of four pieces
+class GameRow
+  attr_reader :farleft, :left, :right, :farright
+
+  def initialize(farleft, left, right, _farright)
+    @farleft = farleft
+    @left = left
+    @right = right
+    @farright = farright
+  end
+end
+
+# These are the pieces for each row
+class GamePieces
+  attr_reader :position, :color
+
+  def initialize(position, color)
+    @position = position
+    @color = color
+  end
+end
+
+# Player and Computer classes
+class Player
+  attr_accessor :role
+
+  def initialize(role)
+    @role = role
+  end
+end
